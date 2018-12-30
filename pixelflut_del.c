8fa78172c74d80d133eeef409c29d2a6f34d5a19
GERZAC1002 <gernot.zacharias@gmx.de>
2018-12-30 23:13:21 +0100

    Upload

--- pixelflut_del.c
+++ pixelflut_del.c
@@ -4,8 +4,8 @@
 
 int main(){
 	while(1){
-		for(int x=1;x<=1920;x++){
-			for(int y=1;y<=1080;y++){
+		for(int x=0;x<=1920;x++){
+			for(int y=0;y<=1080;y++){
 				printf("PX %i %i 000000\n",x,y);
 			}
 		}
